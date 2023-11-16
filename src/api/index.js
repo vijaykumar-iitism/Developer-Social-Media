d3f098c530704131e95131861737ec534289d14a
vijaykumar-iitism <108461672+vijaykumar-iitism@users.noreply.github.com>
2023-11-16 17:42:41 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API =axios.create({baseURL:"http://localhost:5000"});
+const API =axios.create({baseURL:process.env.REACT_APP_NODE_ENV});
 
 API.interceptors.request.use((req) => {
   //if we have profile in local storage ,we send request 
